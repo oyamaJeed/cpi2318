3275ae5440a6b3d7fe21d87059891ba456c6a8dd
Nakahashi <meit2318@kinki-pc.ac.jp>
2023-04-25 02:19:02 -0700

    [ver2]exer_com.h strct_exer1.c mkfile

--- strct_exer1.c
+++ strct_exer1.c
@@ -14,7 +14,7 @@ int main(int argv, char *argc[])
 	printf("struct from=%c\nstruct to=%c\nstruct msg=%s\n",st_val_p->from,st_val_p->to,st_val_p->msg);
 	printf("struct from=%c\nstruct to=%c\nstruct msg=%s\n",(st_val_p+1)->from,(st_val_p+1)->to,(st_val_p+1)->msg);
 
-	st_val_p[1] = (ST_EXER){'A','B',"This is a pen!"};
+	*(st_val_p+1) = (ST_EXER){'A','B',"This is a pen!"};
 
 	printf("***Modify***\n");
         printf("struct from=%c\nstruct to=%c\nstruct msg=%s\n",st_val[0].from,st_val[0].to,st_val[0].msg);
